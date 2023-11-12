892217be1405b997fb7ed0e5d74e8d87927b220d
Ben <bennnnnnnvu@gmail.com>
2023-11-11 16:34:48 -0800

    edit comments/ notes to self

--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -3,6 +3,6 @@ import {all, call} from 'redux-saga/effects';
 import { categoriesSaga } from './categories/category.saga';
 import { userSaga } from './user/user-saga';
 
-export function* rootSaga(){ // root saga automatically listens (?) for categoriesSaga, which drills downwards into the other generator functions that are hookedup
+export function* rootSaga(){ // root saga automatically listens for categoriesSaga + userSaga functions, which drills downwards into the other generator functions that are hooked up
     yield all([call(categoriesSaga), call(userSaga)]);
 };
